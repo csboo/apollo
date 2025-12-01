257d7f90cfc2cfb12d090b61ca7fb8645f1f95c5
Jeromos Kovács <iitsnotme214@proton.me>
2025-12-01 15:24:31 +0100

    fix(backend): working join endpoint

--- src/backend.rs
+++ src/backend.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 use std::{
-    env,
+    env, process,
     sync::{Arc, LazyLock, RwLock},
 };
 
@@ -13,23 +13,40 @@ static EXERCISES: LazyLock<Arc<RwLock<ExerciseState>>> =
 static TEAMS: LazyLock<Arc<RwLock<TeamsState>>> =
     LazyLock::new(|| Arc::new(RwLock::new(TeamsState::new())));
 
+static ADMIN_USERNAME: LazyLock<String> = LazyLock::new(|| {
+    const ADMIN_USERNAME_ENV_VAR_NAME: &str = "APOLLO_MESTER_NEV";
+    let Ok(admin_username) = env::var(ADMIN_USERNAME_ENV_VAR_NAME) else {
+        error!("{ADMIN_USERNAME_ENV_VAR_NAME} env var not set, can't proceed");
+        process::exit(1);
+    };
+    admin_username
+});
+
+static ADMIN_PASSWORD: LazyLock<String> = LazyLock::new(|| {
+    const ADMIN_PASSWORD_ENV_VAR_NAME: &str = "APOLLO_MESTER_JELSZO";
+    let Ok(admin_password) = env::var(ADMIN_PASSWORD_ENV_VAR_NAME) else {
+        error!("{ADMIN_PASSWORD_ENV_VAR_NAME} env var not set, can't proceed");
+        process::exit(2);
+    };
+    admin_password
+});
+
 #[post("/api/join?username")]
-pub async fn join(username: String, password: Option<String>) -> Result<String, ServerFnError> {
+pub async fn join(username: String, password: Option<String>) -> Result<String, HttpError> {
     let teams = &mut TEAMS.write().unwrap();
     (!teams.contains_key(&username)).or_forbidden("already joined")?;
     // trying to join as admin
-    if let Ok(admin_username) = env::var("APOLLO_MESTER_NEV")
-        && admin_username == username
-    {
+    if *ADMIN_USERNAME == username {
         password
             .as_ref()
-            .or_unauthorized("password is required for APOLLO_MESTER")?;
-        env::var("APOLLO_MESTER_JELSZO")
-            .as_ref()
-            .is_ok_and(|apw| apw == &password.unwrap())
-            .or_unauthorized("incorrect or missing password for APOLLO_MESTER")?; // SAFETY: is_some, see above
+            .or_bad_request("password is required for APOLLO_MESTER")?;
+
+        // SAFETY: is_some, see above
+        if *ADMIN_PASSWORD != password.unwrap() {
+            return HttpError::unauthorized("incorrect password for APOLLO_MESTER")?;
+        }
     }
-    teams.insert(username, SolvedState::new()).unwrap();
+    _ = teams.insert(username, SolvedState::new());
     Ok(String::from("helo"))
 }
 
