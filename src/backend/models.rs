8d98bb4bdb41a3e4c06ab0c2430422e56232a885
Jeromos Kovács <iitsnotme214@proton.me>
2025-12-02 11:32:44 +0100

    refactor(server): more concrete models

--- src/backend/models.rs
+++ src/backend/models.rs
@@ -1,5 +1,8 @@
 use std::collections::{BTreeSet, HashMap};
 
-pub type PuzzleSolutions = HashMap<usize, i32>;
-pub type SolvedPuzzles = BTreeSet<usize>;
+pub type PuzzleSolution = i32;
+pub type PuzzleId = usize;
+pub type PuzzleSolutions = HashMap<PuzzleId, PuzzleSolution>;
+/// solved puzzles of a team, or existing puzzles in general
+pub type SolvedPuzzles = BTreeSet<PuzzleId>;
 pub type TeamsState = HashMap<String, SolvedPuzzles>;
