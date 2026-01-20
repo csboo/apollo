1d447d93867014c2d26d792f1309e97063cb7c76
Jeromos Kovács <iitsnotme214@proton.me>
2026-01-20 09:18:16 +0100

    misc(build.rs): warn and default to "unknown" commit hash if no git instead of panicking

--- build.rs
+++ build.rs
@@ -3,16 +3,20 @@
 //! - <https://doc.rust-lang.org/cargo/reference/environment-variables.html>
 //! - <https://doc.rust-lang.org/cargo/reference/build-scripts.html>
 
-use std::env;
 use std::process::Command;
 use std::time::SystemTime;
+use std::{env, io};
 
 fn main() {
-    let git_output = Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output()
-        .unwrap(); // NOTE: probably good enough for now
-    let git_commit_hash = String::from_utf8_lossy(&git_output.stdout);
+    let git_commit_hash = match git_commit_hash() {
+        Ok(hash) => hash,
+        Err(e) => {
+            eprintln!(
+                "WARN: you probably have no `git` installed, couldn't retrieve commit hash: {e}"
+            );
+            String::from("<unknown>")
+        }
+    };
 
     let banner_msg = format!(
         "
@@ -35,7 +39,18 @@ repo: {}
         env!("CARGO_PKG_REPOSITORY"),
     );
 
-    println!("cargo:rustc-env=BANNER={banner_msg:?}");
+    println!("cargo:rustc-env=BANNER={banner_msg:?}"); // escape newlines and quote it
     println!("cargo:rerun-if-changed=.git/HEAD");
     println!("cargo:rerun-if-changed=.git/refs/heads/");
+}
+
+/// get current git commit hash
+/// # Errors
+/// if can't, i.d. no `git` installed
+fn git_commit_hash() -> io::Result<String> {
+    let git_output = Command::new("git")
+        .args(["rev-parse", "--short", "HEAD"])
+        .output()?;
+    let git_commit_hash = String::from_utf8_lossy(&git_output.stdout);
+    Ok(git_commit_hash.to_string())
 }
