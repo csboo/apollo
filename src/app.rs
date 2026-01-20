37eb86f40cadf49f8307978a5f9e74c4aca9d518
Jeromos Kovács <118479592+jarjk@users.noreply.github.com>
2026-01-20 15:09:31 +0100

    CI, CD (#10)
    * ci: add dependabot, text stuff, building, checking, deploying
    
    * ci: release a tip on each push to `main`
    
    * ci: statement-verbosity++
    
    * ci: monthly, not weekly dependabot
    
    * ci: don't check incompatible features together
    
    * chore: fix typos to satisfy ci
    
    * ci(windows): don't forget to extend binary with extension

--- src/app.rs
+++ src/app.rs
@@ -10,6 +10,6 @@ pub fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        "helo"
+        "hello"
     }
 }
